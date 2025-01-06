733ae176bb4f9ed38675f8810ac1cf8731987290
Robert Luiz <robertlsc2016@gmail.com>
2025-01-06 10:35:50 -0300

    fix: update axios instance to use environment-specific base URL

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: "https://rappi-offers-scraping-backend.onrender.com/store/"
-    // process.env.NODE_ENV == "production"
-    //   ? `https://rappi-offers-scraping-backend.onrender.com/store/`
-    //   : `http://localhost:4000/store/`,
+  baseURL:
+    process.env.NODE_ENV == "production"
+      ? `https://rappi-offers-scraping-backend.onrender.com/store/`
+      : `http://localhost:4000/store/`,
 });
 
 export default Axios;
