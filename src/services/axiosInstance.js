c727840b5c853cd07f1ac0e527fdf7b649161685
Robert Luiz <robertlsc2016@gmail.com>
2025-01-09 13:03:04 -0300

    fix: set base URL for axios instance based on environment variable

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const Axios = axios.create({
+  baseURL:
     process.env.NODE_ENV == "production"
       ? process.env.REACT_APP_API_URL
       : `http://localhost:4000/store/`,
