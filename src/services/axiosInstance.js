839a64440703415e67e99d247d231f5dde1b6aa7
Robert Luiz <robertlsc2016@gmail.com>
2025-01-08 21:00:54 -0300

    fix: update axios instance to use environment variable for base URL and add .env to .gitignore

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const Axios = axios.create({
   baseURL:
     process.env.NODE_ENV == "production"
-      ? `https://rappi-offers-scraping-backend.onrender.com/store/`
+      ? process.env.REACT_APP_API_URL
       : `http://localhost:4000/store/`,
 });
 
