699e7e0a492a8b6400e29c4f0b41d65f707274f6
Robert Luiz <robertlsc2016@gmail.com>
2025-01-09 13:02:40 -0300

    fix: correct axios instance base URL configuration for development and production environments

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 const Axios = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-    // process.env.NODE_ENV == "production"
-    //   ? process.env.REACT_APP_API_URL
-    //   : `http://localhost:4000/store/`,
+    process.env.NODE_ENV == "production"
+      ? process.env.REACT_APP_API_URL
+      : `http://localhost:4000/store/`,
 });
 
 export default Axios;
